c6d5c67268f29b985d3d2973dfcd2b1c81b92de4
huajie1121 <979788109@qq.com>
2020-01-02 20:00:15 +0800

    登陆页登陆页静态静态页面

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="login">
-    <div class="login-box"></div>
+  <div class="login-container">
+    <div class="left">
+      <div class="title-box">
+        <img src="../../assets/矢量智能对象 拷贝 9.png" alt />
+        <div class="title">黑马面面</div>
+        <div class="line"></div>
+        <div class="sub-title">用户登录</div>
+      </div>
+      <el-form status-icon ref="ruleForm" class="demo-ruleForm frombox">
+        <el-form-item prop="pass">
+          <el-input type="password" prefix-icon="el-icon-user" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <el-input type="password" prefix-icon="el-icon-lock" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item prop="checkPass" class="frombox1">
+          <el-row>
+            <el-col :span="16"><el-input type="password" prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input></el-col>
+            <el-col :span="8"><img src=""></el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item class="checked-box">
+          <el-checkbox v-model="checked">
+            我已经阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" class="login-button">登录</el-button>
+          <el-button type="primary" class="login-button">注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      checked: false
+    };
+  }
 };
 </script>
 
-<style>
-.login {
+<style lang="less">
+.login-container {
   display: flex;
   /* 侧轴方向 */
   align-items: center;
@@ -25,10 +64,72 @@ export default {
   /* 主轴均分 */
   justify-content: space-around;
   height: 100%;
-}
-.login-box {
-  height: 550px;
-  width: 478px;
-  background-color: #f5f5f5;
+
+  .left {
+    height: 550px;
+    width: 478px;
+    background-color: #f5f5f5;
+
+    padding-top: 44px;
+    // 盒模型 让padding不会撑开盒子
+    box-sizing: border-box;
+    .frombox {
+      width: 394px;
+      height: 45px;
+      margin-top: 31px;
+      margin-left: 43px;
+
+      .frombox1 {
+        margin-bottom: 0px;
+        img{
+          height: 100%;
+          width: 100%;
+          background-color: #fff;
+        }
+        
+      }
+
+      .checked-box {
+        margin-top: 32px;
+        margin-bottom: 28px;
+        font-size: 16px;
+      }
+      .login-button {
+        width: 100%;
+      }
+      .login-button + .login-button {
+        margin: 0;
+        margin-top: 26px;
+      }
+    }
+    .title-box {
+      display: flex;
+      // 上下居中
+      align-items: center;
+      margin-left: 48px;
+
+      img {
+        margin-right: 16px;
+        width: 22px;
+        height: 17px;
+      }
+
+      .title {
+        font-size: 24px;
+        font-weight: 400;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        background-color: #dfdfdf;
+        margin-right: 12px;
+        height: 28px;
+      }
+      .sub-title {
+        font-size: 22px;
+        font-weight: 400;
+      }
+    }
+  }
 }
 </style>
