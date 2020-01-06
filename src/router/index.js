27c562e1883b746ef6dd5768d49c8f2bf65c31a9
huajie1121 <979788109@qq.com>
2020-01-06 17:41:40 +0800

    全部不要为了删除分支而已

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,13 @@ import VueRouter from "vue-router"
 import index from "../views/index/index.vue"
 import login from "../views/login/login.vue"
 
+//导入组件
+import chart from "../views/index/chart/chart.vue"
+import enterprise from "../views/index/enterprise/enterprise.vue"
+import question from "../views/index/question/question.vue"
+import subject from "../views/index/subject/subject.vue"
+import user from "../views/index/user/user.vue"
+
 //注册
 Vue.use(VueRouter)
 
@@ -16,7 +23,29 @@ const router = new VueRouter({
     routes: [
         {
             path: "/index",
-            component: index
+            component: index,
+            children: [
+                {
+                    path: "chart",
+                    component: chart,
+                },
+                {
+                    path: "enterprise",
+                    component: enterprise,
+                },
+                {
+                    path: "question",
+                    component: question,
+                },
+                {
+                    path: "subject",
+                    component: subject,
+                },
+                {
+                    path: "user",
+                    component: user,
+                },
+            ]
         },
         {
             path: "/login",
