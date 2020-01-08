7b8792a91c8d03d5565b297e4d6ae19cb498258c
huajie1121 <979788109@qq.com>
2020-01-08 22:40:22 +0800

    企业页面静态

--- src/views/index/enterprise/enterprise.vue
+++ src/views/index/enterprise/enterprise.vue
@@ -1,13 +1,110 @@
 <template>
-   <div>企业</div>
+  <div>
+    <el-card class="box-card">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="企业编号">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="企业名称">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="创建者">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="formInline.region" placeholder="请选择状态">
+            <el-option label="禁用" value="0"></el-option>
+            <el-option label="启用" value="1"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary">搜索</el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button>清除</el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" icon="el-icon-plus">新增企业</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card class="box-card1">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="序号" width="180"></el-table-column>
+        <el-table-column prop="name" label="企业编码" width="220"></el-table-column>
+        <el-table-column prop="name" label="企业名称" width="220"></el-table-column>
+        <el-table-column prop="name" label="创建者" width="200"></el-table-column>
+        <el-table-column prop="name" label="创建日期" width="220"></el-table-column>
+        <el-table-column prop="address" label="状态"></el-table-column>
+        <el-table-column fixed="right" label="操作" width="250">
+          <template>
+            <el-button type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small">禁用</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      ></el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'enterprise'
-}
+  name: "enterprise",
+  methods: {
+    handleSizeChange(val) {
+      window.console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      window.console.log(`当前页: ${val}`);
+    }
+  },
+  data() {
+    return {
+      currentPage4: 1,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+      formInline: {}
+    };
+  }
+};
 </script>
 
 <style>
-
+.box-card {
+  margin-bottom: 20px;
+}
 </style>
