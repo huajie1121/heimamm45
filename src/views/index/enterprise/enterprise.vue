39410f90f3852361bbbe2f53a107211a56db6e6e
huajie1121 <979788109@qq.com>
2020-01-09 17:36:53 +0800

    企业分页功能和搜索功能

--- src/views/index/enterprise/enterprise.vue
+++ src/views/index/enterprise/enterprise.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <el-card class="box-card">
-      <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="企业编号">
-          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      <el-form :inline="true" :model="formInline" ref="formInline" class="demo-form-inline">
+        <el-form-item label="企业编号" prop="eid">
+          <el-input v-model="formInline.eid"></el-input>
         </el-form-item>
-        <el-form-item label="企业名称">
-          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        <el-form-item label="企业名称" prop="name">
+          <el-input v-model="formInline.name"></el-input>
         </el-form-item>
-        <el-form-item label="创建者">
-          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        <el-form-item label="创建者" prop="username">
+          <el-input v-model="formInline.username"></el-input>
         </el-form-item>
-        <el-form-item label="状态">
-          <el-select v-model="formInline.region" placeholder="请选择状态">
+        <el-form-item label="状态" prop="status">
+          <el-select v-model="formInline.status" placeholder="请选择状态">
             <el-option label="禁用" value="0"></el-option>
             <el-option label="启用" value="1"></el-option>
           </el-select>
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary">搜索</el-button>
+          <el-button type="primary" @click="inquireList">搜索</el-button>
         </el-form-item>
 
         <el-form-item>
-          <el-button>清除</el-button>
+          <el-button @click="clearQuery">清除</el-button>
         </el-form-item>
 
         <el-form-item>
@@ -87,21 +87,44 @@ export default {
     this.enterpriseList();
   },
   methods: {
+    /* 搜索 */
+    inquireList() {
+      this.enterpriseList();
+    },
+    /* 清空搜索内容 */
+    clearQuery() {
+      window.console.log("1");
+      this.$refs.formInline.resetFields();
+      this.enterpriseList();
+    },
+    /* 数据列表 */
     enterpriseList() {
-      enterpriseList({ page: this.page, limit: this.size }).then(res => {
-        window.console.log(res);
+      enterpriseList({
+        page: this.page,
+        limit: this.size,
+        ...this.formInline
+      }).then(res => {
+        // window.console.log(res);
         this.tableData = res.data.data.items;
         this.total = res.data.data.pagination.total;
       });
     },
+    /* 新增 */
     addDialog() {
       this.$refs.addDialog.dialogFormVisible = true;
     },
+    /* 页容量 */
     handleSizeChange(val) {
+      this.page = 1;
+      this.size = val;
+      this.enterpriseList();
       window.console.log(`每页 ${val} 条`);
     },
+    /* 分页 */
     handleCurrentChange(val) {
       window.console.log(`当前页: ${val}`);
+      this.page = val;
+      this.enterpriseList();
     }
   },
   data() {
@@ -111,7 +134,12 @@ export default {
       sizes: [2, 4, 6, 10],
       page: 1,
       tableData: [],
-      formInline: {}
+      formInline: {
+        eid: "",
+        name: "",
+        username: "",
+        status: ""
+      }
     };
   }
 };
